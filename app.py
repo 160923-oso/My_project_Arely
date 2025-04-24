b45936536a6f992369096d7d4bfa1d81bf597a51
Arely <ely1285139@gmail.com>
2025-04-23 23:38:26 -0600

    Mi primer archivo python

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ import streamlit as st
 car_data = pd.read_csv('vehicles_us.csv') # leer los datos
 hist_button = st.button('Construir histograma') # crear un botón
 scatter_button = st.button('Construir gráfico de dispersión') # crear otro botón
+show_histogram = st.checkbox('Mostrar histograma')
+show_scatter = st.checkbox('Mostrar gráfico de dispersión')
         
 if hist_button: # al hacer clic en el botón
     # escribir un mensaje
@@ -26,8 +28,12 @@ if scatter_button: # al hacer clic en el botón del gráfico de dispersión
     # mostrar un gráfico Plotly interactivo
     st.plotly_chart(fig_scatter, use_container_width=True) #aqui va la otra grafica
 
-    # crear una casilla de verificación
-build_histogram = st.checkbox('Construir un histograma')
+if show_histogram:
+    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+    fig_hist = px.histogram(car_data, x="odometer")
+    st.plotly_chart(fig_hist, use_container_width=True)
 
-if build_histogram: # si la casilla de verificación está seleccionada
-    st.write('Construir un histograma para la columna odómetro')
+if show_scatter:
+    st.write('Creación de un gráfico de dispersión para el conjunto de datos de anuncios de venta de coches')
+    fig_scatter = px.scatter(car_data, x="year", y="price")
+    st.plotly_chart(fig_scatter, use_container_width=True)
