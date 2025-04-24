7ad12c38a6d71731647e07dd69443e76c7a40d14
Arely <ely1285139@gmail.com>
2025-04-24 01:41:42 -0600

    Proyecto arely python final

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import streamlit as st
 
 st.header('Datos de anuncios de venta de coches')
 
-st.header('En este proyecto, te proporcionamos un conjunto de datos de anuncios de venta de coches.')
+st.write('En este proyecto, te proporcionamos un conjunto de datos de anuncios de venta de coches.')
 
 car_data = pd.read_csv('vehicles_us.csv') # leer los datos
 hist_button = st.button('Histograma venta') # crear un botón
@@ -26,24 +26,24 @@ if hist_button: # al hacer clic en el botón
 
 if scatter_button: # al hacer clic en el botón del gráfico de dispersión
     # escribir un mensaje
-  st.header('Grafica de dispersion de anuncios de venta de coches')
+  st.header('Grafica de dispersion de anuncios de venta de coches MODELO VS PRECIO')
 
-  st.write('Creación de un gráfico de dispersión para el conjunto de datos de anuncios de venta de coches')
+  st.write('Creación de un gráfico de dispersión para el conjunto de datos de anuncios de venta de coches de acuerdo al modelo y precio')
 
     # crear un gráfico de dispersión
-  fig_scatter = px.scatter(car_data, x="odometer", y="price", color="model")
+  fig_scatter = px.scatter(car_data, x="model", y="price", color="model")
 
     # mostrar un gráfico Plotly interactivo
   st.plotly_chart(fig_scatter, use_container_width=True) #aqui va la otra grafica
 
 if show_histogram:
-    st.header('Histograma de anuncios de modelos de coches')
-    st.write('Creación de un histograma para el conjunto de datos de anuncios de los modelos de coches')
-    fig_hist = px.histogram(car_data, x="model", color="model")
+    st.header('Histograma de anuncios de Año del modelos con su condicion')
+    st.write('Creación de un histograma para el conjunto de datos de anuncios de los modelos de coches y su condicion')
+    fig_hist = px.histogram(car_data, x="model_year", color="condition")
     st.plotly_chart(fig_hist, use_container_width=True)
 
 if show_scatter:
-    st.header('Grafica de dispersion para el conjunto de datos de venta de coches segun el modelo')
-    st.write('Creación de un gráfico de dispersión para el conjunto de datos de venta de coches segun el modelo')
-    fig_scatter = px.scatter(car_data, x="model", y="price" , color="model")
+    st.header('Grafica de dispersion MODELO VS TIPO')
+    st.write('Creación de un gráfico de dispersión para el conjunto de datos de venta de coches segun el modelo junto con el tipo')
+    fig_scatter = px.scatter(car_data, x="model", y="type" , color="model")
     st.plotly_chart(fig_scatter, use_container_width=True)
